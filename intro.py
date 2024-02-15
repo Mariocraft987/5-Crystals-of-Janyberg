9c82f1333247231afa06055735eccb94bbe2f2b7
Mariocraft987 <154646419+Mariocraft987@users.noreply.github.com>
2024-02-15 10:57:54 -0700

    Update and rename levelOne.py to intro.py

--- intro.py
+++ intro.py
@@ -1,4 +1,6 @@
-#Five Crystals of Janyberg
+#Five Crystals of Janyber
+#created by mariocraft987
+
 name = input("Hello fellow adventurer, what's your name?: ")
 
 print("welcome "+name+"!")
@@ -9,4 +11,8 @@ answer = input("Will you do the quest?: ")
 print("I'll just take '"+answer+"' as a yes")
 
 print("Please go to grassland")
-print("and find the first crystal")
+print("and find the first crystal.")
+
+answer1 = input("Your answer:")
+print("Come and find grasslands")
+print("heres the map.")

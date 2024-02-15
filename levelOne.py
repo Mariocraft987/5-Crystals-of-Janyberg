8a837fd83afe2b94da019f8cf1b9a31aadb683b1
Mariocraft987 <154646419+Mariocraft987@users.noreply.github.com>
2024-02-15 08:45:19 -0700

    Create levelOne.py

---
+++ levelOne.py
@@ -0,0 +1,12 @@
+#Five Crystals of Janyberg
+name = input("Hello fellow adventurer, what's your name?: ")
+
+print("welcome "+name+"!")
+print("I am the great wizard of janyberg.")
+print("I have chosen you to find the 5 crystals")
+
+answer = input("Will you do the quest?: ")
+print("I'll just take '"+answer+"' as a yes")
+
+print("Please go to grassland")
+print("and find the first crystal")
